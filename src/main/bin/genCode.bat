889c19a4819e7e26478fba824fc4d906c51c551d
cpaladin <784615815@qq.com>
2016-05-21 21:11:29 +0800

    he

--- src/main/bin/genCode.bat
+++ src/main/bin/genCode.bat
@@ -1,26 +1,22 @@
-set classpath=.;.\lib\*;.\lib\;.\conf\*;.\conf\;.\vm\;.\vm\*;
+set classpath=.;.\lib\*;.\lib\;.\conf\*;.\conf\;
 java -cp %classpath% com.xunlei.game.velocity.GetAllTableMain
 
 ::设置源代码目录，存放生成的源码
 set src=src
 ::设置项目名称，打包生成的源码时，会用到
 set projectName=dayouaccount
+set lib=..\lib\
 
 rd /s /q %src%
 
-::java -cp %classpath% com.xunlei.game.velocity.CreateServletMain -codeSrcDir %src%
-
-java -cp %classpath% com.xunlei.game.velocity.CreateVoMain -codeSrcDir %src%
-
-java -cp %classpath% com.xunlei.game.velocity.CreateDaoMain -codeSrcDir %src%
-
-java -cp %classpath% com.xunlei.game.velocity.CreateDaoFactoryMain -codeSrcDir %src%
-
-java -cp %classpath% com.xunlei.game.velocity.CreateBoMain -codeSrcDir %src%
-
-::java -cp %classpath% com.xunlei.game.velocity.CreatePackageMain -codeSrcDir %src%
+java -cp %classpath% com.xunlei.game.velocity.CodeGenMain -codeSrcDir %src%
 
 ::xcopy /e /y copysrc\* ..\src
 
 call ant -Dsrc=%src% -DprojectName=%projectName% -buildfile %~dp0build-template.xml
+
+::echo d | xcopy /e /y dist\lib-dao\codegen-*-dao.jar %lib%
+::echo d | xcopy /e /y dist\lib-dao\codegen-dao-%projectName%.jar %lib%
+xcopy /e /y dist\lib-dao\codegen-*-dao.jar %lib%
+xcopy /e /y dist\lib-dao\codegen-dao-%projectName%.jar %lib%
 pause
